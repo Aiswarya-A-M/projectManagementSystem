3731956155a416af2c0705a1767f35f3d8885f48
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-12-05 22:53:44 +0530

    routes done

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router=require('./routes/router');
+const app=express();
+app.use(express.json());
+const port =3000;
+app.use('/roles',router);
+
+app.listen(port,()=>{
+    console.log("server running on port",port)
+})
